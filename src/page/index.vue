aa207ed147b5fc169d512e63be34115c36e4ea6e
DESKTOP-PA5T8FP\宋大帅 <1322023401@qq.com>
2018-12-10 23:02:40 +0800

    [core] init front page

---
+++ src/page/index.vue
@@ -0,0 +1,25 @@
+<template>
+    <div>
+        首页展示的东西
+    </div>
+</template>
+
+
+<script>
+
+export default {
+    created () {
+        
+    },
+    mounted() {
+        
+    },
+    data() {
+       return {
+       }
+   },
+    methods:{   
+    },
+    components:{}
+}
+</script>
