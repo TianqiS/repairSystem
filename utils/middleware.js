19acb4354e3f545a1de0be60730a4e2cfefb4095
DESKTOP-PA5T8FP\宋大帅 <1322023401@qq.com>
2018-12-13 00:09:05 +0800

    [middleware] 修改后端error handle

--- utils/middleware.js
+++ utils/middleware.js
@@ -7,18 +7,18 @@ exports.errorHandle = async function (ctx, next) {
     if (typeof error === 'number') {
       if ((error + '').length === 3) ctx.status = error;
       else {
-        ctx.status = 403;
+        ctx.status = 400;
         ctx.body = {
           code: '-2',
           msg: errorList[error] //errorList.json.error
         };
       }
     } else {
+      ctx.status = 500;
       ctx.body = {
         code: '-2',
         msg: '发生未知错误',
       }
-      console.error(error);
     }
   }
 }
