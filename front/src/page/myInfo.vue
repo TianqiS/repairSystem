8095406f2bcc19bb98b5e86f3384a60c5d0ac23e
DESKTOP-PA5T8FP\宋大帅 <1322023401@qq.com>
2018-12-16 11:47:35 +0800

    [myInfo] 添加前端获取用户我的设备数量逻辑

--- front/src/page/myInfo.vue
+++ front/src/page/myInfo.vue
@@ -3,7 +3,7 @@
         <div class="myInfo_top">
             <div class="img"><img src="static/images/icon-touxiang.png"></div>
             <div class="text fl">
-                <h2>稻草</h2>
+                <h2>{{repairmanInfo.name}}</h2>
                 <router-link to="/editMyInfo">编辑</router-link>
             </div>
         </div>
@@ -14,7 +14,7 @@
                         <i class="fa fa-folder-o myInfol"></i>
                         <i class="fa fa-angle-right myInfor"></i>
                         <span>我的设备</span>
-                        <em class="fr">23台</em>
+                        <em class="fr">{{repairmanInfo.devicesCount}}</em>
                     </router-link>
                 </li>
                 <li>
@@ -22,7 +22,6 @@
                         <i class="fa fa-calendar-minus-o myInfol"></i>
                         <i class="fa fa-angle-right myInfor"></i>
                         <span>报修记录</span>
-                        <em class="fr">12/23</em>
                     </router-link>
                 </li>
             </ul>
@@ -38,14 +37,16 @@ export default {
  },
  data() {
   	return {
-       repairmanInfo: {},
+       repairmanInfo: {
+         name: "",
+         devicesCount: 0,
+       },
   	}
   },
   methods:{
       getRepairmanInfo() {
-        this.$api.get('/user/repairmanInfo', "", repairmanInfo => {
-          this.repairmanInfo = repairmanInfo;
-          console.log(repairmanInfo);
+        this.$api.get('/user/repairmanInfo', "", data => {
+          this.repairmanInfo = data.repairmanInfo;
         }, () => {
           this.$router.push('login');
         })
